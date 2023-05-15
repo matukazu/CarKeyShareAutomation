80e90ad14c87852703c19b64117317c8d66513cc
poyokan <kanzaki-trp@outlook.jp>
2023-05-15 23:36:04 +0900

    作業状況保存： 希望乗車時間にアサイン出来なかった人の割合計算処理書いたが、意図した挙動と違ったので後日修正必要

--- src/evaluate.py
+++ src/evaluate.py
@@ -47,22 +47,33 @@ def calc_distance_from_ideal_key_ave(keys_mat):
     return abs(average_have_key - IDEAL_KEY_AVE)
 
 # 希望乗車時間にアサインできなかった人の割合
+# FIX: 希望時間以外にアサインした場合は計算できているが、希望時間にアサインできていない場合が計算出来ていない
+#   userの希望時間から、理想の乗車時間表を作って、値が一致しなかった要素数を計算する形に変更すべき
 def calc_ratio_not_assign_hope_time(times_mat):
 
+    not_equal_hope_time_list = [0] * len(CarUser.user_list)
     # [x]時間帯表を掃いて、どのユーザーがどの時間帯にアサインしたのか調べる → (ユーザーID、アサイン時間)のリスト取得
     ut_list = md.create_user_time_index_list(times_mat)
-    
+    # print("---utlist---")
+    # print(ut_list)
 
     # [ ](ユーザーID、アサイン時間)のリストをForで回す
+
     for ut in ut_list:
-        u_ind = ut[0]
+        u_id = ut[0] + 1
         t_ind = ut[1]
 
-        user = CarUser.get_user_instance(u_ind) # ユーザーIDに一致するユーザーインスタンスを取得する
+        user = CarUser.get_user_instance(u_id) # ユーザーIDに一致するユーザーインスタンスを取得する
         hope_time = user.get_use_time_hope() # 取得したユーザーインスタンスから希望時間インスタンスを取得する
+
         ht_list_ind = hope_time.get_id() - 1 # 希望時間IDは1始まりだが、アサイン時間はリストであり0始まりなので調整
+        print(f"user:{u_id} \t htlistid:{ht_list_ind} \t tind:{t_ind}")
 
-        # [ ] アサイン時間IDと希望時間IDが一致したかT/F判定 → ユーザーごとの判定結果をリストで保存
-    # [ ]ユーザーごとの判定結果リストをもとに、Falseの割合を調べる ※全員希望と外れていたら1、全員希望通りで0になるように。
+        # [ ] あるユーザーがアサイン時間IDと希望時間IDが一致しなかった回数を一覧として算出
+        if (t_ind != ht_list_ind):
+            not_equal_hope_time_list[u_id - 1] += 1 # ユーザーIDは1始まり 配列は0始まりなので調整
 
-    return
+    print(not_equal_hope_time_list)
+    # [ ] 希望時間と一致しなかった回数合計 ÷ 全部希望から外れた場合(＝時間帯表の要素数) ※全員希望と外れていたら1、全員希望通りで0。
+    ratio_not_assign_hope_time = sum(not_equal_hope_time_list) / times_mat.size
+    return ratio_not_assign_hope_time
