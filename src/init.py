31fe73f981c38e5bbddcf25f0aece2a8534d48e1
poyokan <kanzaki-trp@outlook.jp>
2023-05-22 23:57:44 +0900

    初期条件 インスタンス増やしてみた

--- src/init.py
+++ src/init.py
@@ -5,22 +5,33 @@ from MyClass import Car, CarUser, CarUseTime, CarKey
 time_not_use = CarUseTime("乗らない")
 time_18h     = CarUseTime("18時")
 time_19h     = CarUseTime("19時")
+time_20h     = CarUseTime("20時")
 
-user_A = CarUser(name="Aさん", can_drive=True , use_time_hope=time_18h) # 18時希望 運転可
-user_B = CarUser(name="Bさん", can_drive=True , use_time_hope=time_18h) # 18時希望 運転可
-user_C = CarUser(name="Cさん", can_drive=True , use_time_hope=time_19h) # 19時希望 運転可
-user_D = CarUser(name="Dさん", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
-user_E = CarUser(name="Eさん", can_drive=False, use_time_hope=time_19h) # 19時希望 運転不可
-user_F = CarUser(name="Fさん", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
-user_G = CarUser(name="Gさん", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
+user_A = CarUser(name="A", can_drive=True , use_time_hope=time_20h) # 20時希望 運転可
+user_B = CarUser(name="B", can_drive=True , use_time_hope=time_18h) # 18時希望 運転可
+user_C = CarUser(name="C", can_drive=True , use_time_hope=time_18h) # 18時希望 運転可
+user_D = CarUser(name="D", can_drive=True , use_time_hope=time_18h) # 18時希望 運転可
+user_E = CarUser(name="E", can_drive=True , use_time_hope=time_18h) # 18時希望 運転可
+user_F = CarUser(name="F", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
+user_G = CarUser(name="G", can_drive=False, use_time_hope=time_20h) # 20時希望 運転不可
+user_H = CarUser(name="H", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
+user_I = CarUser(name="I", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
+user_J = CarUser(name="J", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
+user_K = CarUser(name="K", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
+user_L = CarUser(name="L", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
+user_M = CarUser(name="M", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
+user_N = CarUser(name="N", can_drive=False, use_time_hope=time_18h) # 18時希望 運転不可
 
-car_A = Car(name="車A", capacity=3)
+car_A = Car(name="車A", capacity=7)
 car_B = Car(name="車B", capacity=5)
+car_C = Car(name="車C", capacity=5)
 
 car_key_A1 = CarKey("カギA1", car_A, can_use_drive=True)
-car_key_A2 = CarKey("カギA2", car_A, can_use_drive=True)
+car_key_A2 = CarKey("カギA2", car_A, can_use_drive=False)
 car_key_B1 = CarKey("カギB1", car_B, can_use_drive=True)
-car_key_B2 = CarKey("カギB2", car_B, can_use_drive=True)
+car_key_B2 = CarKey("カギB2", car_B, can_use_drive=False)
+car_key_C1 = CarKey("カギC1", car_C, can_use_drive=True)
+car_key_C2 = CarKey("カギC2", car_C, can_use_drive=False)
 
 # 個体データ配列作成用
 initial_dict = {
