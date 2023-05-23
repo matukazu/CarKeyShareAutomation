e7bb5f386926e436c7711d7cb69c3341bf45fb19
poyokan <kanzaki-trp@outlook.jp>
2023-05-23 22:47:41 +0900

    個体データ変換処理 アルゴリズムがおかしかった。修正

--- src/individual.py
+++ src/individual.py
@@ -37,12 +37,26 @@ def make_individual():
 
 # 個体データ配列を意味のある表に変換する
 def convert_to_tables_dict(indiv_arr):
+    """ 個体生成用辞書の値から
+    例： 個体1次元配列を配列数3, 6, 14の三つの配列に分割するケース
+    入力 : [4, 1, 2, 8, 5, 2, 3, 10, 1, 4, 4, 3, 2, 2, 2, 4, 3, 1, 1, 4, 1, 4, 1]
+
+    出力
+    ・[4, 1, 2]
+    ・[8, 5, 2, 3, 10, 1]
+    ・[4, 4, 3, 2, 2, 2, 4, 3, 1, 1, 4, 1, 4, 1]
+    """
 
     ret_dict = {}
 
+    arr_start_ind = 0
     for key, values in initial_dict.items():
         arr_total = values["arr_total"]
-        slice_arr = indiv_arr[:arr_total]
+
+        # 配列の長さ分スライスで取り出す。繰り返し時は前回取り出した範囲の次の要素を先頭として取り出す。
+        slice_arr = indiv_arr[arr_start_ind : arr_start_ind + arr_total]
+        arr_start_ind += arr_total
+
         ret_dict[key] = slice_arr
 
     return ret_dict
