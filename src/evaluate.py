2ee73055493521a6ba48a4758346f36dfa455eb9
poyokan <kanzaki-trp@outlook.jp>
2023-05-23 21:11:55 +0900

    車の定員オーバーの有無を評価する関数 設計

--- src/evaluate.py
+++ src/evaluate.py
@@ -41,7 +41,7 @@ def find_total_user_cant_drive_have_driving_key(ku_arr):
 def calc_ratio_not_assign_hope_time(ut_arr):
     """希望乗車時間にアサイン出来なかった人の割合を計算する。
     希望する乗車時間帯表と個体から生成した乗車時間帯表を比べて、異なっている割合を返す。"""
-    
+
     ideal_ut_arr = make_hope_time_table()
 
     diff_count = 0
@@ -53,6 +53,20 @@ def calc_ratio_not_assign_hope_time(ut_arr):
     different_ratio = diff_count / total_elements
 
     return different_ratio
+
+# ある時間帯に乗るユーザーの総数とその時間帯に使用する車の定員合計を比べる
+# 時間帯ごとの車の定員オーバーした数の合計を返す
+def find_user_amount_over_car_capacity(ct_arr, ut_arr):
+
+    # ut_arrからある時間帯に乗る人数を取得する 時間帯ID：合計人数
+    
+    # ct_arrからある時間帯に使う車一覧を取得 時間帯ID：[車一覧]
+    # 車一覧を掃いて、車のインスタンスを取得 → 車の定員を取得 → 合計する → 時間帯ID：定員合計
+    
+    # 時間帯IDごとに 合計人数 - 定員合計を計算 値が0より大きいとき定員オーバーしたとして記録 → 時間帯ID：オーバー人数
+    # すべての時間帯のオーバー人数を合計して返す
+
+    return
 
 # ユーザーのリスト、カギのリストを元にユーザーが希望する理想の乗車時間体表を作成する
 def make_hope_time_table():
